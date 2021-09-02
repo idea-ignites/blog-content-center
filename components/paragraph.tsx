a17bbfca7a95a1b17ed1ad38476457aa67249b04
hsiaofongw <hsiaofong.w@gmail.com>
2021-09-03 01:36:53 +0800

    refactor: 删去冗余文件夹

--- components/paragraph.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export class Paragraph extends React.Component {
-    render() {
-        return <p className="mb-6 mt-6 text-justify">{this.props.children}</p>;
-    }
-}
--- helpers/markdown.ts
+++
@@ -1,5 +0,0 @@
-import { _markdownToReact } from './_markdown';
-
-export function markdownToReact(text: string): React.ReactNode {
-    return _markdownToReact(text) as React.ReactNode;
-}
